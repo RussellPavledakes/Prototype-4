3c45862eaa80a38bc7501869cc084bf9edacbb6b
RussellPavledakes <56165766+RussellPavledakes@users.noreply.github.com>
2020-02-20 08:41:39 -0800

    added a playercontoller, force, and camera

---
+++ Assets/Scripts/PlayerContorller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContorller : MonoBehaviour
+{
+    private Rigidbody PlayerRb;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        PlayerRb.AddForce(Vector3.forward * forwardInput * speed);
+    }
+}
